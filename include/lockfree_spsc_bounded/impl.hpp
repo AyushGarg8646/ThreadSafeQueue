05694cb28ba0d76fdd899cca3ab9d389afe815c2
AyushGarg8646 <ayushgarg211005@gmail.com>
2026-04-15 02:04:17 +0530

    added emplace back and try push

--- include/lockfree_spsc_bounded/impl.hpp
+++ include/lockfree_spsc_bounded/impl.hpp
@@ -3,14 +3,17 @@
 
 #include "defs.hpp"
 
-template <typename T, size_t Capacity>
-using queue = tsfqueue::__impl::lockfree_spsc_bounded<T, Capacity>;
+// template <typename T, size_t Capacity>
+// using queue = tsfqueue::__impl::lockfree_spsc_bounded<T, Capacity>;
 
 template <typename T, size_t Capacity>
 void queue<T, Capacity>::wait_and_push(T value) {}
 
 template <typename T, size_t Capacity>
-bool queue<T, Capacity>::try_push(T value) {}
+bool tsfqueue::__impl::lockfree_spsc_bounded<T, Capacity>::try_push(T value)
+{
+    return emplace_back(std::move(value));
+}
 
 template <typename T, size_t Capacity>
 bool queue<T, Capacity>::try_pop(T &value) {}
@@ -21,7 +24,26 @@ void queue<T, Capacity>::wait_and_pop(T &value) {}
 template <typename T, size_t Capacity>
 bool queue<T, Capacity>::peek(T &value) {}
 
-template <typename T, size_t Capacity> bool queue<T, Capacity>::empty() {}
+template <typename T, size_t Capacity>
+template <typename... Args>
+bool tsfqueue::__impl::lockfree_spsc_bounded<T, Capacity>::emplace_back(Args &&...args) {
+  size_t tail_cur = tail.load(std::memory_order_acquire);
+  size_t next_tail = (tail_cur + 1) % capacity;
+  if (next_tail == head_cache) {
+    head_cache = head.load(std::memory_order_acquire);
+    if (next_tail == head_cache) {
+      return false;
+    }
+  }
+  arr[tail_cur] = T(std::forward<Args>(args)...);
+  tail.store(next_tail, std::memory_order_release);
+  return true;
+}
+template <typename T, size_t Capacity>
+bool tsfqueue::__impl::lockfree_spsc_bounded<T, Capacity>::empty()
+{
+    return (head.load(std::memory_order_relaxed) ==tail.load(std::memory_order_relaxed));
+}
 
 #endif
 
